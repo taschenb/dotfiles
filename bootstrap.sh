b4f905482a37a314f70b9e507b1634a92b54bafd
taschenb <taschenb@posteo.de>
2017-05-09 09:16:03 +0200

    bootstrap: Add uninstallation process
    Quickly remove any dotfiles previously bootstraped.
    Mostly for systems you access temporarily and want to leave clean.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
 # Create symlinks to the dotfiles in this directory based on a specific set.
-# Usage: ./bootstrap.sh <set>
+# Usage (install): ./bootstrap.sh <set>
+# Usage (uninstall): ./bootstrap.sh uninstall
 
 
 # Sets:
@@ -25,25 +26,115 @@ set_awesome="config/awesome/"
 dotfile_dir=$(dirname $(readlink -f "$0"))
 # Backup existing dotfiles to this directory before replacing them
 backup_dir=~/.dotfiles_backup
+# Track all symlinked files
+installed_files="$backup_dir/.installed_files"
+# Track all created directories
+installed_dirs="$backup_dir/.installed_dirs"
+
+
+install () {
+    set_to_install=$1
+
+    if [ ! -d "$backup_dir" ]; then
+        echo "Creating backup directory $backup_dir to store existing files"
+        mkdir -p $backup_dir
+    fi
+
+    for file in $set_to_install; do
+        echo "Processing ~/.$file"
+
+        # If there is already a symlink there, delete it
+        if [ -h "$HOME/.file" ]; then
+            echo "Deleting old symlink"
+            rm "$HOME/.$file"
+        fi
+
+        # If it is a file, back it up if not already done so
+        if [ -f "$HOME/.$file" ] && [ -d $backup_dir ]; then
+            echo "Backing up existing file"
+            mv -i "$HOME/.$file" "$backup_dir/"
+        fi
+
+        # Create the directory the symlink is going to be in
+        directory=$(dirname "$HOME/.$file")
+        if [ ! -d "$directory" ] ; then
+            echo "Creating missing directory $directory"
+            mkdir -p "$directory"
+            echo "$directory" >> "$installed_dirs"
+        fi
+
+        echo "Creating symlink"
+        ln -s "$dotfile_dir/$file" "$HOME/.$file"
+        echo "$HOME/.$file" >> "$installed_files"
+        echo ""
+    done
+}
+
+uninstall() {
+    # Remove all created symlinks and put any backuped files back
+    if [ -f "$installed_files" ]; then
+        while read -r line; do
+            file="$line"
+            if [ -h "$file" ]; then
+                echo "Remove symlink $file..."
+                rm "$file"
+            fi
+            if [ -f "$backup_dir/$file" ]; then
+            echo "Found backup of $file. Put it back..."
+            mv -i "$backup_dir/$file" "$file"
+            fi
+        done < "$installed_files"
+
+        rm $installed_files
+    fi
+
+    # Finally, remove created directories
+    if [ -f $installed_dirs ]; then
+        while read -r line; do
+            dir="$line"
+            if [ -d "$dir" ]; then
+                echo "Removing previously created directory $dir"
+                if [ "$(find "$dir" | wc -l)" -gt 0 ]; then
+                    echo "WARNING: DIRECTORY IS NOT EMPTY"
+                fi
+                rm -ri "$dir"
+            fi
+        done < "$installed_dirs"
+
+        rm $installed_dirs
+    fi
+
+    # Cleanup the backup_dir itself
+    if [ "$(find "$backup_dir" | wc -l)" -gt 0 ]; then
+        echo "Seems you have still files in $backup_dir..."
+        echo "Review and delete manually"
+    else
+        echo "Removing $backup_dir..."
+        rm -r "$backup_dir"
+    fi
+}
 
 
 case "$1" in
     full)
-        install=$set_full
+        install "$set_full"
         git config --global init.templatedir "$HOME/git_template"
         ;;
     basic)
-        install=$set_basic;;
+        install "$set_basic";;
     git_template)
-        install=$set_git_template
+        install "$set_git_template"
         git config --global init.templatedir "$HOME/.git_template"
         ;;
     bspwm)
-        install=$set_bspwm;;
+        install "$set_bspwm";;
     awesome)
-        install=$set_awesome;;
+        install "$set_awesome";;
+    uninstall)
+       uninstall;;
     *)
-        printf "%s\n" "Usage:bootstrap.sh <set>"
+        printf "%s\n" "Usage (install): bootstrap.sh <set>"
+        printf "%s\n\n" "Usage (uninstall): bootstrap.sh uninstall"
         printf "%s\n%s\n %s\n%s\n %s\n%s\n %s\n%s\n %s\n%s\n %s\n" \
                "Available sets:" \
                "full:" \
@@ -58,37 +149,3 @@ case "$1" in
                "Setup awesome"
         exit 1;;
 esac
-
-
-if [ ! -d "$backup_dir" ]; then
-    echo "Creating backup directory $backup_dir to store existing files"
-    mkdir -p $backup_dir
-fi
-
-
-for file in $install; do
-    echo "Processing ~/.$file"
-
-    # If there is already a symlink there, delete it
-    if [ -h ~/.$file ]; then
-        echo "Deleting old symlink"
-        rm ~/.$file
-    fi
-
-    # If it is a file, back it up if not already done so
-    if [ -f ~/.$file ] && [ -d $backup_dir ]; then
-        echo "Backing up existing file"
-        mv -i ~/.$file $backup_dir/
-    fi
-
-    # Create the directory the symlink is going to be in
-    directory=$(dirname ~/.$file)
-    if [ ! -d $directory ] ; then
-        echo "Creating missing directory $directory"
-        mkdir -p $directory
-    fi
-
-    echo "Creating symlink"
-    ln -s $dotfile_dir/$file ~/.$file
-    echo ""
-done
